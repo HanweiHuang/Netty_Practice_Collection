895a9572eb06b7c64f0204abee6743ef8cd0dbd6
harveyNorman <wei2215038@gmail.com>
2016-09-06 00:38:06 +1000

    rename package

--- src/main/java/com/harvey/helloworld/EchoClient.java
+++ src/main/java/com/harvey/helloworld/EchoClient.java
@@ -1,4 +1,4 @@
-package com.harvey.chapter01;
+package com.harvey.helloworld;
 
 import java.net.InetSocketAddress;
 
--- src/main/java/com/harvey/helloworld/EchoClientHandler.java
+++ src/main/java/com/harvey/helloworld/EchoClientHandler.java
@@ -1,4 +1,4 @@
-package com.harvey.chapter01;
+package com.harvey.helloworld;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
