ca3129fd6b5fa095a233a74095ad9a9b04010cec
harveyNorman <wei2215038@gmail.com>
2016-09-05 21:36:57 +1000

    chapter01 example

---
+++ src/main/java/com/harvey/chapter01/EchoClientHandler.java
@@ -0,0 +1,44 @@
+package com.harvey.chapter01;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * ByteBuf（Netty的字节容器）
+ * @author HarveySally
+ * @date Sep 5, 2016
+ * @time 4:03:43 PM
+ */
+@Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf>{
+
+	/**
+	 * 数据从服务器接受后调用
+	 */
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, ByteBuf in)
+			throws Exception {
+		System.out.println("Client received: " + in.toString(CharsetUtil.UTF_8));
+	}
+	/**
+	 * 服务器连接被建立后调用
+	 */
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!",CharsetUtil.UTF_8));
+	}
+	
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+			throws Exception {
+		
+		cause.printStackTrace();
+		ctx.channel();
+	}
+
+
+}
