d111f48d8585f3fadf4284a853a3db81bf7808b4
harveyNorman <wei2215038@gmail.com>
2016-09-06 20:41:16 +1000

    Transport example

---
+++ src/main/java/com/harvey/transport/NettyNioServer.java
@@ -0,0 +1,99 @@
+package com.harvey.transport;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.util.Random;
+
+import com.harvey.helloworld.EchoServer;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.CharsetUtil;
+
+public class NettyNioServer {
+
+	public void server(int port) throws InterruptedException{
+		
+		final ByteBuf buf = Unpooled.unreleasableBuffer(
+                Unpooled.copiedBuffer("Hi!\r\n", Charset.forName("UTF-8")));
+		
+		NioEventLoopGroup group = new NioEventLoopGroup();
+		ServerBootstrap serverBootStrap = null;
+		try {
+			serverBootStrap = new ServerBootstrap();
+			serverBootStrap.group(group);
+			
+			serverBootStrap.channel(NioServerSocketChannel.class)
+			.localAddress(new InetSocketAddress(port))
+			.childHandler(new ChannelInitializer<SocketChannel>() {
+
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+//						@Override
+//						public void channelActive(ChannelHandlerContext ctx)
+//								throws Exception {
+//							ctx.writeAndFlush(buf.duplicate())
+//								.addListener(ChannelFutureListener.CLOSE);
+//						}
+						
+						@Override
+						public void channelRead(ChannelHandlerContext ctx, Object msg)
+								throws Exception {
+							
+							ByteBuf in = (ByteBuf)msg;
+				
+							for (int i = 0; i < in.capacity(); i++) {
+							    byte b = in.getByte(i);
+							    System.out.println((char) b);
+							}
+							//System.out.println("Server received: " + in.toString(CharsetUtil.UTF_8));
+							ctx.write(buf);
+						}
+						@Override
+						public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+							//冲刷所有待审消息到远程节点。添加一个future 监听，当关闭通道后，操作完成 
+							ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+							.addListener(ChannelFutureListener.CLOSE);
+						}
+						@Override
+						public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+								throws Exception {
+							cause.printStackTrace();
+							ctx.close();
+						}
+					});
+				}
+				
+			});
+			ChannelFuture cf = serverBootStrap.bind().sync();
+			System.out.println(NettyNioServer.class.getName()+": started and listening to" + cf.channel().localAddress());
+			cf.channel().closeFuture().sync();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			group.shutdownGracefully().sync();
+		}
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		try {
+			new NettyNioServer().server(19000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/com/harvey/transport/PlainOioServer.java
@@ -0,0 +1,55 @@
+package com.harvey.transport;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+/**
+ * use java api achieve an implement of I/O without netty.
+ * 1.bind serversockert with port
+ * 2.waiting for the connection
+ * 3.open a new thread for write out "HI" to server
+ * 4.close socket
+ * @author HarveySally
+ * @date Sep 6, 2016
+ * @time 12:47:12 AM
+ */
+public class PlainOioServer {
+	
+	public void serve(int port) throws IOException{
+		try {
+			final ServerSocket serverSocket = new ServerSocket(port);//1
+			for(;;){
+				final Socket clientSocket = serverSocket.accept(); //2
+				
+				new Thread(new Runnable() {
+					
+					public void run() {
+						OutputStream out;
+						try {
+							out = clientSocket.getOutputStream();
+							out.write("hello world".getBytes(Charset.forName("UTF-8")));
+							out.flush();
+							clientSocket.close();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} finally{
+							try {
+								clientSocket.close();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					}
+				}).start();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
